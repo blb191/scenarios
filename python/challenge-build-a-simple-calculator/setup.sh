ad907c174b3b4d889b97a7175ef0372ae5dc3a12
blb191 <1354492342@qq.com>
2023-03-23 20:40:17 +0800

    Add challenge-build-a-simple-calculator

--- python/challenge-build-a-simple-calculator/setup.sh
+++ python/challenge-build-a-simple-calculator/setup.sh
@@ -1,8 +1,1 @@
 wget https://cdn.jsdelivr.net/gh/labex-labs/common-scripts@master/python-shell-history/.setup-python-shell-history.sh&&zsh .setup-python-shell-history.sh
-
-#!/bin/zsh
-
-touch /home/labex/project/calculator_unittest.py
-
-# Install the unittest package using pip
-pip install unittest
